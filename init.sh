2bfffc1806f5333478e3423a900410119f85ed39
crcms <crcms@crcms.cn>
2019-03-27 21:42:33 +0800

    docker-compose and docker without sudo

--- init.sh
+++ init.sh
@@ -104,7 +104,11 @@ sudo tee /etc/docker/daemon.json <<-'EOF'
 EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
-
+# without sudo
+sudo usermod -aG docker $USER
+# docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 ############# laradock ######################
 mkdir ~/env
