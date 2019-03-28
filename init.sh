954633f9777cfbad19459efe470ee4e465e22c18
simon <crcms@crcms.cn>
2019-03-28 11:46:05 +0800

    fix sougoupingying link error

--- init.sh
+++ init.sh
@@ -60,7 +60,7 @@ sudo dpkg -i xunlei.deb
 
 ############# 输入法 ######################
 # 搜索拼音
-wget http://cdn2.ime.sogou.com/dl/index/1524572264/sogoupinyin_2.2.0.0108_amd64.deb?st=X7oHrcyiKc9UivRgyAN9SA&e=1553615383&fn=sogoupinyin_2.2.0.0108_amd64.deb -O suogoupingyin.deb
+wget http://cdn2.ime.sogou.com/dl/index/1524572264/sogoupinyin_2.2.0.0108_amd64.deb -O suogoupingyin.deb
 sudo dpkg -i suogoupingyin.deb
 sudo apt-get install -f
 sudo dpkg -i suogoupingyin.deb
